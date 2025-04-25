0ab1ef2eca58f3b1c47b68cd4ea94c3f778e88e1
downstreamspiral <hemendrakumararya0@gmail.com>
2025-04-26 00:21:32 +0530

    move

--- server.js
+++ server.js
@@ -1,27 +1,58 @@
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const fingerprintJsServerApi = require("@fingerprintjs/fingerprintjs-pro-server-api");
+const { Sequelize, Op } = require("sequelize");
+const bcrypt = require("bcrypt");
+const UserModel = require("./models/User");
+const DeviceFingerprintModel = require("./models/DeviceFingerprint");
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-// const mongoose require
-const uri = "mongodb+srv://hemendrakumararya:bbbb4@cluster0.3hhzwk7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const app = express();
+const PORT = 5000;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+// Initialize the Fingerprint Server API client instance
+const client = new fingerprintJsServerApi.FingerprintJsServerApiClient({
+  apiKey: "SECRET_API_KEY",
+  region: fingerprintJsServerApi.Region.Global,
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+// Initialize sequelize with SQLite database connection
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./db/database.db",
+});
+
+// Create model instances
+const User = UserModel(sequelize);
+const DeviceFingerprint = DeviceFingerprintModel(sequelize);
+
+// Set the view engine to ejs
+app.set("view engine", "ejs");
+
+// Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+// Define routes
+// Display the registration page
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+// Display the dashboard
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+// Function to start the server
+function startServer() {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 }
-run().catch(console.dir);
+
+(async () => {
+  await sequelize.sync({ alter: true });
+
+  console.log("Model synchronized successfully");
+
+  startServer();
+})();
